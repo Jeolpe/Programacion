e1dc70b534a7873456d972a33d925d6907999747
Jesús María Olalla Pérez <72356032+Jeolpe@users.noreply.github.com>
2020-10-14 21:15:09 +0200

    Add files via upload

---
+++ Ejercicio de clase/Bandera/Calculos/introducir.java
@@ -0,0 +1,28 @@
+/*
+ *@utor Jesús María Olalla Pérez 
+ */
+import java.util.Scanner;
+
+public class introducir {
+	public static void main (String args[]) {
+    
+    Scanner s = new Scanner(System.in);
+    
+    
+    System.out.println("Escribe el primer número");
+    int num_uno = s.nextInt();
+    
+    System.out.println("Escribe el segundo número");
+    int num_dos = s.nextInt();
+    
+    
+    int resultado_multiplicacion = 0;
+    resultado_multiplicacion = num_uno * num_dos;
+    
+    
+    System.out.println("");
+    System.out.println("El resultado de la suma es " + resultado_multiplicacion);
+    
+	}
+}
+
