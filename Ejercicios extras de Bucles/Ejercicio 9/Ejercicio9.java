fb406ffbe495358388e59b79c9f1626b41646461
Jesús María Olalla Pérez <72356032+Jeolpe@users.noreply.github.com>
2020-10-24 13:51:32 +0200

    Add files via upload

---
+++ Ejercicios extras de Bucles/Ejercicio 9/Ejercicio9.java
@@ -0,0 +1,43 @@
+/*
+ *@utor Jesús María Olalla Pérez
+ */
+
+import java.util.Scanner;
+public class Ejercicio9 {
+	
+	public static void main (String args[]) {
+		
+    Scanner s = new Scanner(System.in);
+      int respuesta = 0;
+      int total = 0;
+    System.out.println("introduzca un número entero ( 0 para salir)");
+    respuesta = s.nextInt();
+    
+      
+    if( respuesta > 0){
+    System.out.println("El cuadrado de " + respuesta + " es " + (respuesta * respuesta));
+    System.out.println("");
+    
+    for ( int i = 1; i < 4; i++){
+    System.out.println("introduzca un número entero ( 0 para salir)");
+    respuesta = s.nextInt();
+    
+    if( respuesta > 0){
+    System.out.println("El cuadrado de " + respuesta + " es " + (respuesta * respuesta));
+    System.out.println("");
+      
+  
+   } else {
+     System.out.println("Usted ha salido correctamente");
+    // esto es para finalizar el bucle// 
+    i = -2;
+  }
+     }
+   
+    
+     } else {
+     System.out.println("Usted ha salido correctamente");
+    
+    }
+	}
+}
