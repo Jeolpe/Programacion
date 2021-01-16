0b04e75730d6a512bdc0f787dc580cfa587232b0
Jesús María Olalla Pérez <Jesusm.olalla.alu@iescampanillas.com>
2021-01-16 23:39:14 +0100

    Agregar archivos mediante carga

---
+++ Ejercicio del libro Tema 6/Ejercicio 4/Ejercicio4.java
@@ -0,0 +1,20 @@
+/*
+ *@jesús María Olalla Pérez 
+ */
+
+import java.util.Scanner;
+public class Ejercicio4 {
+	
+	public static void main (String args[]) {
+    
+    Scanner s = new Scanner(System.in);
+    int a = 0;
+    
+    for(int i = 1; i <= 20; i++){
+      a = (int)(Math.random()*11);
+      System.out.print(a + " ");
+      }
+		
+	}
+}
+
