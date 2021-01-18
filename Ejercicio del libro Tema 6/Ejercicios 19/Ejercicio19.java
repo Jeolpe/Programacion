887b11c9ad53dda3f0436589a8f54fd6c7470277
Jesús María Olalla Pérez <Jesusm.olalla.alu@iescampanillas.com>
2021-01-18 21:22:14 +0100

    Agregar archivos mediante carga

---
+++ Ejercicio del libro Tema 6/Ejercicios 19/Ejercicio19.java
@@ -0,0 +1,44 @@
+/*
+ *@utor Jesús María Olalla Pérez 
+ */
+
+import java.util.Scanner;
+public class Ejercicio19 {
+	
+	public static void main (String args[]) {
+    
+    Scanner s = new Scanner(System.in);
+    int a = 0;
+    int contador = 0;
+    int M = -101;
+    int m =201;
+    int suma = 0;
+    int resultado = 0;
+    
+    for(int i = 1; i <= 50; i++){
+    a = (int)(Math.random()*301)-100;
+    
+    if(a % 2 == 0){
+    if(a > M){
+      M = a;
+      }
+    }else{
+      
+      if(a < m){
+       m = a;
+        }
+      }
+      
+        contador++;
+        suma+= a;
+        System.out.print(a + " ");
+  }
+  resultado = suma / contador;
+  System.out.println("");
+  System.out.println("El número más alto de los pares es: " + M);
+  System.out.println("El número más bajo de los impares es: " + m);
+  System.out.println("La media de todos los números son: " + resultado);
+		
+	}
+}
+
