1623faf5866c4755b627f0094f5e90134cc8f2f7
Jesús María Olalla Pérez <Jesusm.olalla.alu@iescampanillas.com>
2021-01-20 21:51:13 +0100

    Agregar archivos mediante carga

---
+++ Ejercicios del libro Tema 7.1/Ejercicio 12/Ejercicio12.java
@@ -0,0 +1,43 @@
+/*
+ *@utor Jesús Maía Olalla Pérez 
+ */
+
+import java.util.Scanner;
+public class Ejercicio12 {
+	
+	public static void main (String args[]) {
+    
+    Scanner s = new Scanner(System.in);
+		int[]n = new int[10];
+    int[]r = new int[10];
+    int r1 = 0;
+    int r2 = 0;
+    
+    
+    for(int i = 0; i < n.length; i++){
+      n[i] = s.nextInt();
+      r[i] = n[i];
+      }
+      System.out.println("");
+      System.out.print("Escribe el primer valor: ");
+      r1 = s.nextInt();
+      System.out.print("Escribe el segundo valor: ");
+      r2 = s.nextInt();
+      
+      r[r2] = r[r1];
+      
+      for(int i = r2 + 1; i < n.length; i++){
+        r[i] = n[i-1];
+        }
+        
+        r[0] = n[9];
+        for(int i = 0; i < r1; i++){
+          r[i+1] = n[i];
+          }
+          
+          for(int num:r){
+            System.out.print(num + " ");
+            }
+	}
+}
+
