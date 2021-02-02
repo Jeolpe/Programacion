c8a4e7fb3dab63135570cb9e2052dda5391ed214
Jesús María Olalla Pérez <Jesusm.olalla.alu@iescampanillas.com>
2021-02-02 23:21:39 +0100

    Agregar archivos mediante carga

---
+++ Ejercicios del libro Tema 8/Ejercicio 1 al 14/Ejercicio10.java
@@ -0,0 +1,24 @@
+/*
+ *@utor Jesús María Olalla Pérez 
+ */
+import Matematicas.Primary;
+import java.util.Scanner;
+
+public class Ejercicio10 {
+	
+	public static void main (String args[]) {
+    
+    Scanner sc = new Scanner(System.in);
+    int n    = 0;
+    int kill = 0;
+    
+    System.out.print("Escribe un número entero positivo: ");
+    n = sc.nextInt();
+    
+    System.out.print("Escribe la cantidad de número que dea eliminar por la izquierda: ");
+    kill = sc.nextInt();
+    
+    System.out.print("Número decapitado: " + Primary.quitaDigitoPorDelante(n,kill));
+		
+	}
+}
