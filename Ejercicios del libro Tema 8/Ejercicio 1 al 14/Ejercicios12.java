91731f628887b052f6b475bfa096bb5ff8510144
Jesús María Olalla Pérez <Jesusm.olalla.alu@iescampanillas.com>
2021-02-02 23:25:59 +0100

    Agregar archivos mediante carga

---
+++ Ejercicios del libro Tema 8/Ejercicio 1 al 14/Ejercicios12.java
@@ -0,0 +1,25 @@
+/*
+ *@utor Jesús María Olalla Pérez 
+ */
+import Matematicas.Primary;
+import java.util.Scanner;
+
+public class Ejercicios12 {
+	
+	public static void main (String args[]) {
+    
+    Scanner sc = new Scanner(System.in);
+    int n    = 0;
+    int digi = 0;
+    
+    System.out.print("Escribe un número entero positivo: ");
+    n = sc.nextInt();
+    
+    System.out.print("Escribe el dígito que quiera añadir por delante al número: ");
+    digi = sc.nextInt();
+    
+    System.out.print("El número con el dígito añadido es el: " + Primary.pegaDigitoPorDelante(n,digi));
+		
+	}
+}
+
